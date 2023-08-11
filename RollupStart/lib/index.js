ff8ba36aa2d7d7aad4a24d51b62f6f1007c2cee6
不想名字 <1060610431@qq.com>
2023-08-11 10:58:23 +0800

    first commit

---
+++ RollupStart/lib/index.js
@@ -0,0 +1,2 @@
+!function(e,l){e&&!e.getElementById("livereloadscript")&&((l=e.createElement("script")).async=1,l.src="//"+(self.location.host||"localhost").split(":")[0]+":35729/livereload.js?snipver=1",l.id="livereloadscript",e.getElementsByTagName("head")[0].appendChild(l))}(self.document),function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";console.log("hale最开心ss"),alert("开发"),console.log("development")}));
+//# sourceMappingURL=index.js.map
